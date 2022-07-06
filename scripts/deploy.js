270ff20c351b42fbcf083c0cbc049813c88dee68
Hussaini Muhammad Auwal <hussain@Hussains-MacBook-Pro.local>
2022-07-06 09:36:59 +0100

    contracts deployed to cronos mainnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,14 @@
 const { ethers, upgrades } = require("hardhat");
-const factory = require("../artifacts/contracts/MockERC20.sol/MockERC20.json");
-
+let admin;
 async function main() {
-  const [admin, user] = await ethers.getSigners();
-  // const RanceTreasury = await ethers.getContractFactory("RanceTreasury");
+  [admin] = await ethers.getSigners();
+  const RanceTreasury = await ethers.getContractFactory("RanceTreasury");
   const RanceProtocol = await ethers.getContractFactory("RanceProtocol");
-  // const MockERC20 = await ethers.getContractFactory("MockERC20");
-  const rance = new ethers.Contract(
-    process.env.RANCE_TOKEN,
-    factory.abi,
-    admin
-  );
-  const addr = "0x39A12f5704E52277c9c9949bD4663e3ACaC4807c";
-  const musd = new ethers.Contract(process.env.MUSD, factory.abi, admin);
-
-  await rance.approve(addr, ethers.utils.parseUnits("10000"));
-  await musd.approve(addr, ethers.utils.parseUnits("10000"));
-  await rance.transfer(addr, ethers.utils.parseUnits("10000"));
-  await musd.transfer(addr, ethers.utils.parseUnits("10000"));
-  // const rance = await MockERC20.deploy("Rance Token", "RANCE");
-  /* const protocol = await upgrades.deployProxy(
+  const treasury = await RanceTreasury.deploy(admin.getAddress());
+  const protocol = await upgrades.deployProxy(
     RanceProtocol,
     [
-      process.env.TREASURY,
+      treasury.address,
       process.env.UNISWAP_ROUTER,
       process.env.RANCE_TOKEN,
       process.env.MUSD,
@@ -33,14 +19,17 @@ async function main() {
   await treasury.setInsuranceProtocolAddress(protocol.address);
   await protocol.setTreasuryAddress(treasury.address);
   await protocol.addInsureCoins(
-    ["BTC", "ETH", "CRO"],
-    [process.env.BTC, process.env.ETH, process.env.CRO]
+    ["BTC", "ETH", "CRO", "MMF"],
+    [process.env.BTC, process.env.ETH, process.env.CRO, process.env.MMF]
   );
   await protocol.transferOwnership(process.env.ADMIN_ADDRESS);
+  await treasury.addAdmin(process.env.ADMIN_ADDRESS);
 
   console.log(`
-    RanceProtocol deployed to: ${protocol.address}`); */
+    RanceProtocol deployed to: ${protocol.address},
+    RanceTreasurt deployed to: ${treasury.address}`);
 }
+module.exports = [admin.getAddress()];
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
